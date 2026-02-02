f3cbdc935af3a33e776ad7653564d82006a56ec2
MsBookGurl <152986472+MsBookGurl@users.noreply.github.com>
2026-02-03 00:12:34 +1200

    removed create | alter procedure - giving errors i cannot understand, data loaded tho

--- second-de-tut/scripts/bronze/proc_load_bronze.sql
+++ second-de-tut/scripts/bronze/proc_load_bronze.sql
@@ -16,9 +16,11 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+
+	DECLARE @start_time DATETIME, 
+	@end_time DATETIME,
+	@batch_start_time DATETIME, 
+	@batch_end_time DATETIME; 
 	BEGIN TRY
 		SET @batch_start_time = GETDATE();
 		PRINT '================================================';
@@ -34,7 +36,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Ficto\Downloads\second-tutorial\second-de-tut\datasets\source_crm\cust_info'
+		FROM 'C:\Users\Ficto\Downloads\second-tutorial\second-de-tut\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -138,4 +140,3 @@ BEGIN
 		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
 		PRINT '=========================================='
 	END CATCH
-END
